2458c59b79b2d58d8cf387a59f35961203568e52
JY <yupandaria@outlook.com>
2024-01-16 16:31:26 +0800

    Modify physics check & time counter for boar.

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -8,7 +8,7 @@ public class Enemy : MonoBehaviour
 {
     protected Rigidbody2D rb;
     protected Animator anim;
-    
+    private PysicsCheck pysicsCheck;
     [Header("Properties")]
     public float normalSpeed;
     public float chaseSpeed;
@@ -18,6 +18,7 @@ public class Enemy : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        pysicsCheck = GetComponent<PysicsCheck>();
         currentSpeed = normalSpeed;
     }
 
@@ -29,6 +30,11 @@ public class Enemy : MonoBehaviour
     private void Update()
     {
         faceDirection = new Vector3(-transform.localScale.x,0,0);
+        
+        if(pysicsCheck.touchLeftWall || pysicsCheck.touchRightWall)
+        {
+            transform.localScale = new Vector3(-transform.localScale.x,transform.localScale.y,transform.localScale.z);
+        }
     }
 
     public virtual void Move()
--- Assets/Scripts/General/PysicsCheck.cs
+++ Assets/Scripts/General/PysicsCheck.cs
@@ -5,22 +5,46 @@ using UnityEngine;
 
 public class PysicsCheck : MonoBehaviour
 {
+    private CapsuleCollider2D coll;
     [Header("Parameters")]
+    public bool manual;
+    public Vector2 bottomOffset;
+    public Vector2 leftOffset;
+    public Vector2 rightOffset;
     public float checkRadius;
     public LayerMask groundLayer;
     [Header("Status")]
     public bool isGrounded;
+
+    public bool touchLeftWall;
+    public bool touchRightWall;
     public void Update()
     {
         Check();
     }
 
+    private void Awake()
+    {
+        coll = GetComponent<CapsuleCollider2D>();
+        if (!manual)
+        {
+            rightOffset = new Vector2(coll.bounds.size.x/2 + coll.offset.x,coll.offset.y);
+            leftOffset = new Vector2(-(coll.bounds.size.x/2 - coll.offset.x), rightOffset.y);
+        }
+    }
+
     public void Check()
     {
-        isGrounded = Physics2D.OverlapCircle(transform.position, checkRadius, groundLayer);
+        isGrounded = Physics2D.OverlapCircle((Vector2)transform.position + bottomOffset, checkRadius, groundLayer);
+        rightOffset = new Vector2((coll.bounds.size.x/2+coll.offset.x*transform.localScale.x),coll.offset.y);
+        leftOffset = new Vector2(-(coll.bounds.size.x/2-coll.offset.x*transform.localScale.x),rightOffset.y);
+        touchLeftWall = Physics2D.OverlapCircle((Vector2)transform.position + leftOffset, checkRadius, groundLayer);
+        touchRightWall = Physics2D.OverlapCircle((Vector2)transform.position + rightOffset, checkRadius, groundLayer);
     }
     private void OnDrawGizmosSelected()
     {
-        Gizmos.DrawWireSphere(transform.position, checkRadius);
+        Gizmos.DrawWireSphere((Vector2)transform.position + bottomOffset, checkRadius);
+        Gizmos.DrawWireSphere((Vector2)transform.position + leftOffset, checkRadius);
+        Gizmos.DrawWireSphere((Vector2)transform.position + rightOffset, checkRadius);
     }
 }
